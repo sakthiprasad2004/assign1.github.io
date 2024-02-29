04965f7dbe93c946e31acb6654c25c65238346c2
sakthiprasad2004 <sakthiprasad.m2022ece@sece.ac.in>
2024-02-29 15:34:22 +0530

    calculator

---
+++ egs1.js
@@ -0,0 +1,24 @@
+// function leapyear(year):
+//   return(year%4==0 && year%100!==0 )||( year%400==0);
+// try{
+//     var datestr="29-02-2024"
+//     var arr1=datestr.split("-");
+//     var inputdate=parseInt(arr1[0]);
+//     var inputmonth=parseInt(arr1[1]);
+//     var inputyear=parseInt(arr1[2]);
+//     if(isNaN(inputday) || isNaN(inputmonth || is NaN()))
+// }    
+let a=3;
+let b=5;
+let c=15;
+function(a)
+   if(a==3)
+     stra= console.log("hello")
+    return stra;
+function(b)
+    if(b==5)
+      strb=conole.log("world");
+    return strb;
+if((a%c)&&(b%c)==0)
+   console.log(function(a)+function(b));
+  
